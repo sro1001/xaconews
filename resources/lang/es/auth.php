f4907d874c7adeafa1049767cb4a12e943813190
Sergio Ruiz <sro1001@alu.ubu.es>
2023-05-10 18:24:24 +0200

    Añadidos mensajes de validación en español

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Las credenciales de acceso son inválidas.',
+    'throttle' => 'Demasiados intentos de acceso. Prueba de nuevo en :seconds segundos.',
+
+];
