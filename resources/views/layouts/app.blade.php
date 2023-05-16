a83a9c02ad52c044c3f7791b37cbd801639122f6
Sergio Ruiz <sro1001@alu.ubu.es>
2023-05-16 18:50:29 +0200

    Mejora en el layout de la aplicación + Logo e imágenes en el Login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div id="app" style="background-image: url('{{ asset('img/xacoNewsLogin2.jpg')}}');height: 100vh;">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background: #1b330c !important;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img width="220" src="{{asset('img/logoLogin.png')}}" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
