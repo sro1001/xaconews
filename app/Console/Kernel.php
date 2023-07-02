901d08555cf3d23ab3eb5d33030bbaa79c05c85d
Sergio Ruiz <sro1001@alu.ubu.es>
2023-07-02 22:00:44 +0200

    Corrección de texto logout + sintaxis kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('noticias:sincronizar')->dailyAt('03:00');;
+        $schedule->command('noticias:sincronizar')->dailyAt('03:00');
     }
 
     /**
